5b770aee38e8e1094c45d5c6c77a0a36f16b3080
Matheus Queiroz dos Santos <matheusqueirozds@gmail.com>
2022-07-22 22:36:07 -0300

    Removi o app.css

--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { createGlobalStyle } from "styled-components";
 import App from "./App";
+
+const GlobalStyle = createGlobalStyle`
+*{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+
+a{
+    color: inherit;
+    text-decoration: none;
+}
+`;
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
+        <GlobalStyle />
         <App />
     </React.StrictMode>
 );
