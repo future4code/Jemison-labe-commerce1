5a7313c460045f66e074e03d8eadca0916164535
Daniel da silva de sousa <94769388+DanielSiilva@users.noreply.github.com>
2022-07-24 17:45:30 -0300

    refazendo o projeto

---
+++ src/Components/Footer/StyledFooter.js
@@ -0,0 +1,74 @@
+
+import styled from "styled-components";
+
+
+export const FooterContainer = styled.footer`
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
+        Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+`;
+
+export const FooterTopBg = styled.div`
+    background-color: #ffffff;
+    padding: 30px;
+`;
+
+export const FooterTop = styled.div`
+    max-width: 1130px;
+    margin: 0 auto;
+`;
+
+export const FooterTopInforme = styled.p`
+    background-color: #ffc107;
+    padding: 13px;
+    border-radius: 5px;
+    line-height: 1.05;
+`;
+
+export const FooterIconesBg = styled.div`
+    display: flex;
+    justify-content: space-between;
+    padding: 30px 0;
+`;
+
+export const FooterIconesTitulo = styled.p`
+    margin-bottom: 15px;
+`;
+
+export const BandeirasLista = styled.ul`
+    display: flex;
+    align-items: center;
+    gap: 20px;
+    list-style: none;
+`;
+
+export const FooterContatoBG = styled.div`
+    background-color: #f7f7f7;
+    padding: 36px;
+`;
+
+export const FooterContato = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+    max-width: 1130px;
+    margin: 0 auto;
+`;
+
+export const FooterSocial = styled.ul`
+    display: flex;
+    align-items: center;
+    gap: 25px;
+`;
+
+export const FooterBottomBg = styled.div`
+    background-color: #3b3a38;
+    color: #ffffff;
+    padding: 42px;
+`;
+
+export const FooterBottom = styled.div`
+    display: flex;
+    justify-content: space-between;
+    max-width: 1130px;
+    margin: 0 auto;
+`;
