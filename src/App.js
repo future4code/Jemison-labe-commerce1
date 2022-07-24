7f552fa1b7d3935128369e6322f5fd250ada56c1
Daniel da silva de sousa <94769388+DanielSiilva@users.noreply.github.com>
2022-07-24 16:49:32 -0300

    R-Projeto

--- src/App.js
+++ src/App.js
@@ -1,40 +1,127 @@
 import React, { useState } from "react";
-import styled from "styled-components";
+
+
+import {Carrinho} from "./components/Carrinho/Carrinho"
+import { Filtro } from "./components/Filtros/Filtro";
+import { Main } from "./components/Main/Main";
 import "./App.css";
-import {bancoDeDadosList} from "./data/bancoDeDados"
+import styled from "styled-components";
 
-import Header from "./Components/Header/Header";
-import Main from "./Components/Main/Main";
-import Footer from "./Components/Footer/Footer";
+import listaDeProdutos from "./components/listaDeProdutos"
+import { Header } from "./components/Header/Header";
+import {Footer} from "./components/Footer/Footer"
 
+export const ConteinerPrincipal = styled.div `
+  display: grid;
+  grid-template-columns: 1fr 3fr 1fr ;
+  padding: 3rem;
+  justify-items: center;
 
-
-export const Conteudo = styled.main`
-    display: flex;
-    justify-content: space-around;
-    gap: 20px;
-    padding: 20px;
-    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
-        Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
-    width: 100%;
-`;
+  
+`
 
 function App() {
 
-    
-    return (
-        <div>
-            <Header />
+  const [minValor, setMinValor] = useState(-Infinity);
+  const [maxValor, setMaxValor] = useState(Infinity);
+  const [pesquisa, setPesquisa] = useState("");
+  const [pesquisaHeader, setPesquisaHeader] = useState("")
+  const [ordemCrescent, setOrdemCrescent] = useState("descrecente")
 
-            <Conteudo>
-                <Main 
-                    titulo="Camisetas Espaciais"
-                />
-            </Conteudo>
+  const [carrinho, setCarrinho] = useState([])
+  //[{item, quantidadeItens }]
 
-            <Footer />
-        </div>
-    );
+  const addItensAoCarrinho = (id) =>{
+      const inicioCarrinho = carrinho.findIndex((item) => {
+        return item.id === id;
+      });
+      // Verifica se o carrinho está fazio, se sim add o item!
+      if( inicioCarrinho < 0) {
+        const novoItem ={
+          ...listaDeProdutos.find((item) => item.id === id),
+          quantidadeItens: 1,
+        }; // se o carrriho estiver vazio,  a condição copia o banco de dados, usa a funcção Find, para buscar no array de objetos, uma unica propriedades comum entre os objetos que é o id, e compara com os id do carrinho.. Assim, adciona mais um elemento
+
+        const carrinhoNovo = [...carrinho, novoItem];
+        setCarrinho(carrinhoNovo) /// Variavel que faz uma copia do carrinho, que estará vazio e adiciona o a constante novoItem. E atualiza o carrinho atravez do setCarrinho, passando a variavel novoCarrinho somando mais um
+      } else {
+        const novoCarrinho = carrinho.map(item =>{
+          if (item.id === id){
+            return {...item, quantidadeItens: item.quantidadeItens + 1}
+          }
+
+          return item;
+        }); // Condição  faz o mapeamento do stato inicial do carrinho depois de atualizado pela primeira condição. Passamos como pararmetro o id do item que está no carrinho, se o id for igual, então somamos mais um!
+
+        setCarrinho(novoCarrinho)
+      }
+  }
+
+  const removeItensDoCarrinho = (id) =>{
+    const novoCarrinho = carrinho.map((listaDeProdutos) =>{
+      if(listaDeProdutos.id === id) {
+        return {...listaDeProdutos, quantidadeItens: listaDeProdutos.quantidadeItens -1 };
+      }
+      return listaDeProdutos;
+    })
+
+    .filter((listaDeProdutos) => listaDeProdutos.quantidadeItens > 0);
+    setCarrinho(novoCarrinho)
+
+
+
+  }
+
+ 
+
+
+
+  return (
+    <>
+      <Header 
+          pesquisaHeader = {pesquisaHeader}
+          onChangePesquisaHader= {(e) =>  setPesquisaHeader(e.target.value)}
+      />
+
+        <ConteinerPrincipal>
+            
+              <Filtro
+                minValor = {minValor}
+                onChangeMinValor = {(e) => setMinValor(e.target.value)}
+
+                maxValor = {maxValor}
+                onChangeMaxValor = {(e) => setMaxValor(e.target.value)}
+
+                pesquisa = {pesquisa}
+                onChangePesquisa = {(e) => setPesquisa(e.target.value)}
+              />
+
+              <Main
+
+                listaDeProdutos = {listaDeProdutos}
+
+                minValor = {minValor}
+                maxValor = {maxValor}
+                pesquisa = {pesquisa}
+                pesquisaHeader = {pesquisaHeader}
+                
+                ordemCrescent = {ordemCrescent}
+                onChangeOrdemCrescent = {(e) => setOrdemCrescent(e.target.value)}
+
+                addItensAoCarrinho = {addItensAoCarrinho}
+
+              />
+
+
+              <Carrinho
+                carrinho = {carrinho}
+                setCarrinho = {setCarrinho}
+                removeItensDoCarrinho = {removeItensDoCarrinho}
+              />
+        </ConteinerPrincipal>
+    <Footer></Footer>
+    </>
+  )
 }
 
 export default App;
--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -1,123 +1,83 @@
-import React, { useState } from "react";
-import styled from "styled-components";
-import Filtros from "../Filtros/Filtros";
-import Carrinho from "../Carrinho/Carrinho";
-import { Conteudo } from "../../App";
-import {bancoDeDadosList} from "../../data/bancoDeDados"
+import React from "react";
 
-import Produto from "../Main/Produto";
+import {MainPrincipal} from "./StyleMain"
 
-const Titulo = styled.h2`
-  font-size: 2rem;
-`;
 
-const MainInformacoes = styled.div`
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: space-between;
-  margin-bottom: 40px;
-`;
 
-const SelecionaOrdemProdutos = styled.select`
-    color: #757575;
-    font-size: 0.875rem;
-    padding: 5px 10px;
-    border-radius: 4px;
-    border: none;
-`;
 
-const Item = styled.div`
-  display: flex;
-  flex-direction: column;
-`;
+import {ConteinerPrincipal, Produto, BotaoCard , NomeProduto,  PrecoProduto, GridProdutos } from './StyleMain'
 
-const Produtos = styled.div`
-  display: grid;
-  grid-template-columns: repeat(3, 250px);
-  gap: 20px;
-`;
 
-function Main(props) {
-  //Filtros
-  const [minValor, setMinValor] = useState(-Infinity);
-  const [maxValor, setMaxValor] = useState(Infinity);
-  const [pesquisa, setPesquisa] = useState("");
 
-  //Ponto de otimização
-  const lowerPesquisa = pesquisa.toLowerCase();
 
-  const HandleMinValor = (e) => {
-      setMinValor(e.target.value)
-  }
+export function Main (props) {
 
-  const HandleMaxValor = (e) => {
-      setMaxValor(e.target.value)
-  }
+    const obterListaFiltrada = () => {
+         return props.listaDeProdutos
 
-  const HandlePesquisa = (e) => {
-      setPesquisa(e.target.value)
-  }
+         .filter((listaDeProdutos) => props.minValor <= listaDeProdutos.precoProduto || props.minValor === "")
 
-  return (
-    <Conteudo>
-      <Filtros 
-        pesquisa={pesquisa}
-        minValor={minValor}
-        maxValor={maxValor}
-        setPesquisa={setPesquisa}
-        setMinValor={setMinValor}
-        setMaxValor={setMaxValor}
+         .filter((listaDeProdutos) => props.maxValor >= listaDeProdutos.precoProduto || props.maxValor === "")
+
+         .filter((listaDeProdutos) => listaDeProdutos.nomeProduto.toLowerCase().includes(props.pesquisa.toLowerCase()))
+
+         .filter ((listaDeProdutos) => listaDeProdutos.nomeProduto.toLowerCase().includes(props.pesquisaHeader.toLowerCase()))
         
-      />
-      <main>
-        <Titulo>{props.titulo}</Titulo>
-
-        <MainInformacoes>
-          <p>Exibindo 1–6 de 6 resultados</p>
-
-          <SelecionaOrdemProdutos name="ordenacao" id="ordenacao">
-            <option value="crescente" selected>
-              Ordenar por preço: menor para maior
-            </option>
-            <option value="decrescente">
-              Ordenar por preço: maior para menor
-            </option>
-          </SelecionaOrdemProdutos>
-        </MainInformacoes>
-
-        <Produtos>
-            {bancoDeDadosList
-            .filter((bandoDeDadosInicial) => { //lowerPesquisa - utilizado para otimizar o código
-                return bandoDeDadosInicial.nomeProduto.toLowerCase().includes(lowerPesquisa) || bandoDeDadosInicial.descricaoProduto.toLowerCase().includes(lowerPesquisa)
-            })
-            .filter((bandoDeDadosInicial) => {
-                return bandoDeDadosInicial.precoProduto >= minValor || minValor === ""
-            })
-            .filter((bandoDeDadosInicial) => {
-                return bandoDeDadosInicial.precoProduto <= maxValor || maxValor === ""
-            })
-            .map((bandoDeDadosInicial) => {
-                return (
-                    <Produto
-                    key={bandoDeDadosInicial}
-                    id={bandoDeDadosInicial.id}
-                    fotoProduto={bandoDeDadosInicial.fotoProduto}
-                    descricaoProduto={bandoDeDadosInicial.descricaoProduto}
-                    nomeProduto={bandoDeDadosInicial.nomeProduto}
-                    precoProduto={`R$ ${bandoDeDadosInicial.precoProduto}`}
-                    botaoProduto={bandoDeDadosInicial.botaoProduto}
-                >
-                    {" "}
-                </Produto>
-                )
-            })
-
+         .sort((maior, menor) => {
+            if(props.ordemCrescent === "descrecente"){
+                return (maior.precoProduto - menor.precoProduto)
+            } else {
+                return (menor.precoProduto - maior.precoProduto)
             }
-        </Produtos>
-      </main>
-      <Carrinho />
-    </Conteudo>
-  );
-}
+         })
+      };
 
-export default Main;
+
+
+     const listaFiltrada = obterListaFiltrada().map((listaDeProdutos) => {
+        return (
+            <Produto>
+                <img src={listaDeProdutos.fotoProduto}></img>
+                <NomeProduto> {listaDeProdutos.nomeProduto}</NomeProduto>
+                <PrecoProduto>{listaDeProdutos.precoProduto}</PrecoProduto>
+                <BotaoCard  onClick={() => props.addItensAoCarrinho(listaDeProdutos.id)}> 
+                    Adcionar ao Carrinho
+                
+                </BotaoCard>
+                
+            </Produto>
+        )
+
+     }) 
+
+
+    return (
+        <MainPrincipal>
+            <h1> Camisetas Espacias</h1>
+            <ConteinerPrincipal>
+                <p>Quantidade de produtos: <span>{listaFiltrada.length} </span></p>
+
+                <select 
+                    value={props.ordemCrescent}
+                    onChange = {props.onChangeOrdemCrescent}
+                >
+
+                    <option value={"crescente"}> Crescente</option>
+                    <option value={"descrecente"}> Descrescente</option>
+
+                </select>
+
+            </ConteinerPrincipal>
+
+            <GridProdutos>
+                {listaFiltrada}
+            </GridProdutos>
+
+
+
+
+        </MainPrincipal>
+        
+    )
+
+}
